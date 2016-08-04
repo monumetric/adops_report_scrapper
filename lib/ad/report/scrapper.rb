fcc221e6356d476da8f4bb8ff8308981b439afed
Stayman Hou <stayman.hou@gmail.com>
2016-08-04 10:17:05 -0400

    skeleton

---
+++ lib/ad/report/scrapper.rb
@@ -0,0 +1,9 @@
+require "ad/report/scrapper/version"
+
+module Ad
+  module Report
+    module Scrapper
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ad/report/scrapper/version.rb
@@ -0,0 +1,7 @@
+module Ad
+  module Report
+    module Scrapper
+      VERSION = "0.1.0"
+    end
+  end
+end
