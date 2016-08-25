5cadb1e1087820c5dc107627ba1dfbb69677eeaa
Stayman Hou <stayman.hou@gmail.com>
2016-08-25 08:17:55 -0400

    0.1.6 adtechus

--- Rakefile
+++ Rakefile
@@ -7,7 +7,7 @@ require 'adops_report_scrapper'
 require 'byebug'
 
 desc 'Collect all data'
-task :all => [:openx, :tremor, :brightroll, :yellowhammer, :adaptv, :fourninefive, :adx, :revcontent, :gcs, :browsi, :netseer, :sonobi, :nativo, :adsupply, :marfeel, :adsense, :criteo, :triplelift, :conversant, :liveintent, :adiply, :contentad, :facebookaudience] do # openx is the most unstable one, run it first
+task :all => [:openx, :tremor, :brightroll, :yellowhammer, :adaptv, :fourninefive, :adx, :revcontent, :gcs, :browsi, :netseer, :sonobi, :nativo, :adsupply, :marfeel, :adsense, :criteo, :triplelift, :conversant, :liveintent, :adiply, :contentad, :facebookaudience, :adtechus] do # openx is the most unstable one, run it first
   puts '========== You are all set'
 end
 
@@ -124,6 +124,11 @@ end
 desc 'Collect facebookaudience data'
 task :facebookaudience do
   save_as_csv :facebookaudience, :facebookaudience
+end
+
+desc 'Collect adtechus data'
+task :adtechus do
+  save_as_csv :adtechus, :adtechus
 end
 
 def date
---
+++ lib/adops_report_scrapper/adtechus_client.rb
@@ -0,0 +1,54 @@
+require 'date'
+require_relative 'base_client'
+
+class AdopsReportScrapper::AdtechusClient < AdopsReportScrapper::BaseClient
+  private
+
+  def login
+    @client.visit 'http://marketplace.adtechus.com'
+    @client.fill_in 'Username', :with => @login
+    @client.fill_in 'Password', :with => @secret
+    @client.click_button 'Sign in'
+    begin
+      @client.find :css, '#mainwindow'
+    rescue Exception => e
+      raise e, 'Adtechus login error'
+    end
+  end
+
+  def scrap
+    request_report
+  end
+
+  def request_report
+    @client.visit(@client.find(:css, '#mainwindow')[:src])
+    wait_for_loading
+    report_id = @client.find(:xpath, '//tr[./td/div/span[text()="Placement fill rate report"]]')[:id]
+    report_id = report_id.tr 'row_', ''
+    @client.visit "https://marketplace.adtechus.com/h2/reporting/showReport.do?action=showreportpage._._.#{report_id}"
+    @client.within_frame @client.find(:css, '#reportwindow') do
+      @client.within_frame @client.find(:xpath, '//iframe[@name="uid_2"]') do
+        extract_data_from_report
+      end
+    end
+  end
+
+  def extract_data_from_report
+    rows = @client.find_all :xpath, '//table[@class="table"]/tbody/tr'
+    rows = rows.to_a
+    rows.pop
+    @data = rows.map { |tr| tr.find_css('td,th').map { |td| td.visible_text } }
+  end
+
+  def wait_for_loading
+    18.times do |_i| # wait 3 min
+      begin
+        @client.find(:xpath, '//*[contains(text(),"loading")]')
+      rescue Exception => e
+        break
+      end
+      sleep 3
+    end
+    sleep 1
+  end
+end
