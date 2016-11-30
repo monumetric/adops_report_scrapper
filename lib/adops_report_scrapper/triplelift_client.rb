3100e3a87325e37926dfb91ccde6ca8891b1bb19
Stayman Hou <stayman.hou@gmail.com>
2016-11-30 10:58:22 -0500

    0.1.34

--- lib/adops_report_scrapper/triplelift_client.rb
+++ lib/adops_report_scrapper/triplelift_client.rb
@@ -20,61 +20,34 @@ class AdopsReportScrapper::TripleliftClient < AdopsReportScrapper::BaseClient
     @date_str = @date.strftime('%B %d, %Y')
     @client.find(:xpath, '//*[text()="Reporting"]').click
     wait_for_spin
-    # byebug
-    # @client.find(:xpath, '//button[contains(text(),"Last 7 days")]').click
-    # @client.find(:xpath, '//a[contains(text(),"Yesterday")]').click
-
-
+    @client.find(:xpath, '//button[contains(text(),"Last 7 days")]').click
+    @client.find(:xpath, '//a[contains(text(),"Yesterday")]').click
+    sleep 1
     
-    @client.find(:xpath, '//button[../../div[contains(text(),"Publisher")]]').click
-    @publishers = @client.find_all(:xpath, '//*[@ng-click="selectPublisher(pub)"]').to_a.map { |pub_elem| pub_elem.text(:all) }
-    sleep 2
-    @publishers = @client.find_all(:xpath, '//*[@ng-click="selectPublisher(pub)"]').to_a.map { |pub_elem| pub_elem.text(:all) }
-    @client.find(:xpath, '//button[../../div[contains(text(),"Publisher")]]').click
-    @data = []
-    while @publishers.count > 0
-      extract_data(@publishers.shift)
-    end
+    @client.find(:xpath, '//tl-checkbox/div/span[text()[normalize-space()="Placement"]]').click
+    sleep 1
+    @client.find(:xpath, '//tl-checkbox/div/span[text()[normalize-space()="Clicks"]]').click
+    sleep 1
+    @client.find(:xpath, '//*[text()[normalize-space()="Run query"]]').click
+    sleep 10
+
+    extract_data
   end
 
-  def extract_data(publisher)
-    10.times do
-      @client.find(:xpath, '//*[text()="Reporting"]').click
-      sleep 2
-      @client.find(:xpath, '//button[../../div[contains(text(),"Publisher")]]').click
-      index = -1 - @publishers.count(publisher)
-      sleep 1
-      @client.find_all(:xpath, "//*[text()=\"#{publisher}\"]")[index].click
-      wait_for_spin
+  def extract_data
+    @data = []
+    return if @client.find_all(:xpath, '//*[text()="No data available for selected date range"]').count > 0
 
-      return if @client.find_all(:xpath, '//*[text()="No data available for selected date range"]').count > 0
-
-      @client.find(:xpath, '//*[@model="startDate"]//input').set @date_str
-      sleep 1
-      @client.find(:xpath, '//*[@model="endDate"]//input').set @date_str
-      sleep 1
-      @client.find(:xpath, '//button[../../div[contains(text(),"Group by")]]').click
-      @client.find(:xpath, '//*[text()="Date and Placement"]').click
-      wait_for_spin
-
-      return if @client.find_all(:xpath, '//*[text()="No data available for selected date range"]').count > 0
-
-      rows = @client.find_all :xpath, '//table/*/tr'
-      rows = rows.to_a
-      rows.shift
-      header = rows.shift
-      n_data = rows.map { |tr| tr.find_css('td,th').map { |td| td.visible_text } }
-      if n_data[-1][1].empty?
-        @client.evaluate_script 'window.location.reload()'
-        next
-      end
-      if @data.count == 0
-        n_header = header.find_css('td,th').map { |td| td.visible_text }
-        @data << n_header
-      end
-      @data += n_data
-      break
+    rows = @client.find_all :xpath, '//table/*/tr'
+    rows = rows.to_a
+    rows.pop
+    header = rows.shift
+    n_data = rows.map { |tr| tr.find_css('td,th').map { |td| td.visible_text } }
+    if @data.count == 0
+      n_header = header.find_css('td,th').map { |td| td.visible_text }
+      @data << n_header
     end
+    @data += n_data
   end
 
   def wait_for_spin
--- lib/adops_report_scrapper/version.rb
+++ lib/adops_report_scrapper/version.rb
@@ -1,3 +1,3 @@
 module AdopsReportScrapper
-  VERSION = "0.1.33"
+  VERSION = "0.1.34"
 end
